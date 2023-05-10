878280cf165c218d671c99a400d79e6453e014d1
IvanKahinga <ikahinga@gmail.com>
2023-05-10 16:38:10 +0300

    Updated subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,31 +1,40 @@
 class SubscriptionsController < ApplicationController
   before_action :authorize_request
+  # GET /subscriptions
+    # Show all subscriptions and articles in the subscribed categories
+    def index
+      current_user = @current_user
+      subscriptions = current_user.subscriptions.includes(category: :articles)
+      render json: subscriptions, include: { category: { include: :articles } }
+    end
+
 
     # POST /subscriptions Subscribe to article categories
   
     def create
-      current_user = User.find_by(id: (@current_user_id))
+      current_user = @current_user
       if current_user
-        params[:category_ids].each do |cat_id|
-          subscription = Subscription.create(user: current_user, category_id: cat_id.to_i)
-        end
-        render json: subscription, status: :accepted, notice: "Subscriptions saved successfully."
+          subscription = Subscription.create(user_id: current_user.id, category_id: params[:category_id])
+          render json: subscription, status: :accepted, notice: "Subscriptions saved successfully."
       else
         render json: { error: "User not found" }, status: :unprocessable_entity
       end
     end
         
-    # DELETE /subscription Unsubscribe from category
+   
+        # Unsubscribe from category
     def destroy
-      current_user = User.find_by(id: @current_user_id)
-      subscription = Subscription.find(params[:id])
-      if current_user && current_user == subscription.user
+      current_user = @current_user
+      category = Category.find(params[:id])
+      subscription = current_user.subscriptions.find_by(category_id: category.id)
+      
+      if subscription
         subscription.destroy
         head :no_content
-      else 
-        render json: { error: "Subscription not found or unauthorized access" }, status: :unprocessable_entity
+      else
+        render json: { error: "Subscription not found" }, status: :unprocessable_entity
       end
     end
-       
+          
 
 end
